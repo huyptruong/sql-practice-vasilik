762d602d7cd9d316a18f77a0ff20b665944bddd6
huyptruong <kennytruongpham@gmail.com>
2025-02-15 12:10:01 -0800

    minor format fixes

--- intro_problems/04-sales_representatives_in_the_united_states.sql
+++ intro_problems/04-sales_representatives_in_the_united_states.sql
@@ -1,6 +1,6 @@
 select e.FirstName as first_name
-	 , e.LastName as last_name
-	 , e.HireDate as hire_date
+     , e.LastName as last_name
+	   , e.HireDate as hire_date
   from Employees as e
 where e.Title = 'Sales Representative'
   and e.Country = 'USA'
--- intro_problems/05-orders_placed_by_specified_employee_id.sql
+++ intro_problems/05-orders_placed_by_specified_employee_id.sql
@@ -1,4 +1,4 @@
 select o.OrderID as order_id
-	 , o.OrderDate as order_date
+     , o.OrderDate as order_date
   from Orders as o
-where o.EmployeeID = 5
+ where o.EmployeeID = 5
