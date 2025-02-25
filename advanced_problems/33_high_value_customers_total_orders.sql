59ffb90e7fa0e49620dccdbb18643427ee80076e
huyptruong <kennytruongpham@gmail.com>
2025-02-24 17:24:18 -0800

    added advanced problems 30 - 35

---
+++ advanced_problems/33_high_value_customers_total_orders.sql
@@ -0,0 +1,12 @@
+select o.CustomerID as customer_id
+     , c.CompanyName as company_name
+     , total_order_amount = sum(od.UnitPrice * od.Quantity)
+  from Orders as o
+  join OrderDetails as od
+    on o.OrderID = od.OrderID
+  join Customers as c
+    on c.CustomerID = o.CustomerID
+ where year(o.OrderDate) = 2016
+ group by o.CustomerID, c.CompanyName
+having sum(od.UnitPrice * od.Quantity) >= 15000
+ order by total_order_amount desc
